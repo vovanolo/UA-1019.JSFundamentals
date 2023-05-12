4e12d9103bee21b6e4d3b4fbe21dab0f300e7050
vovanolo <vpankiv@softserveinc.com>
2023-05-12 19:46:23 +0300

    first topic

---
+++ app.js
@@ -0,0 +1,74 @@
+// console.log("hello everyone!");
+// коментар
+
+// console.log("hello everyone!");
+// console.log("hello world!"); 
+
+
+// var let const 
+
+// let message = "hello world!";
+
+// message = "something";
+
+// const message = "hello world!";
+
+// message = "something";
+
+// console.log(message);
+
+// let message;
+
+// message = "hello world!";
+
+// let one = "hello";
+// let two = 25;
+
+// one = two;
+
+// console.log(one);
+
+// const one = 0.1;
+// const two = 0.2;
+
+// const one = "hello";
+// const two = "world";
+
+// console.log(one + " " + two);
+
+// let user = {
+//     name: "ivan",
+//     surname: "ivanenko",
+//     age: 25,
+//     info: null,
+//     newInfo: undefined
+// };
+
+// console.log(user);
+
+// let x = "hello";
+
+// x = 25;
+
+// let info = true;
+
+// console.log(typeof(info));
+
+// alert("hello world!");
+
+// let name  = prompt("Please enter your name");
+
+// console.log(name);
+
+// let ok = confirm("Are you ok?");
+
+// console.log(ok);
+
+// let name  = prompt("Please enter your name");
+
+// alert("Welcome " + name);
+// alert(`Welcome ${name}`);
+
+
+
+
