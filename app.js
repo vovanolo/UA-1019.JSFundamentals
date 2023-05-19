540d71adae7db5c3289b8d7262148534bbdb934b
vovanolo <vpankiv@softserveinc.com>
2023-05-19 16:05:34 +0300

    second lesson

--- app.js
+++ app.js
@@ -71,4 +71,109 @@
 
 
 
+// let a = 2;
+
+// a = a + 1;
+
+// a = ++a;
+
+
+// 2 ** 2;
+
+// Math.pow(2, 3)
+
+// Math.sqrt(2)
+
+// let a = "hello";
+// let b = "world";
+
+// let a = 5;
+// let b = "2";
+
+// console.log(5 + "4" + 5);
+
+// let a = 10;
+
+// a*=10;
+
+
+// console.log(10 < 50);
+// console.log(10 != 11);
+
+// console.log(11 == 1);
+
+// console.log("a" > "A");
+
+// true - 1, "sfsf"
+// false - 0, ""
+
+// console.log(0 == null);
+
+// let age = prompt("Please enter your age: ");
+// let check = age>=18 && age > 100 && age >1000;
+
+// let result = (check) ? "you are adult" : "you are notAdult";
+
+// console.log(result);
+
+// result = "you are notAdult"
+
+// const users = ["Ivan", "Petro", "Vasya"];
+
+// push
+// users.splice(1, 1, "Kolya");
+
+// console.log(users);
+// users.push("Pedro", "Kolya");
+// users.pop();
+
+// console.log(users);
+
+// users = 5;
+
+// console.log(users.length); // кількість елементів в масиві
+// users[1] = "Dmytro";
+// users[3] = "Maria";
+
+// users[7] = "John";
+
+
+// console.log(users[5]);
+
+// console.log(users[1]);
+
+// if, else, if else, switch
+
+let age = +prompt("How old are you?");
+
+// if(age >= 18){
+//     console.log("you are adult");
+// } else{
+//     console.log("you are notadult");
+// }
+
+// if(age >= 18 && age <=30){
+//     console.log("you are normal age");
+// }else if(age >= 31 && age <= 60){
+//     console.log("you are nice age");
+// }else if(age < 0){
+//     console.log("Wrong value");
+// } else{
+//     console.log("you are notadult");
+// }
+
+// switch(true){
+//     case age >= 18 && age <=30:
+//         console.log("you are 18");
+//         break;
+//     case 20:
+//         console.log("you are 20");    
+//         break;
+// }
+
+
+
+
+
+
 
