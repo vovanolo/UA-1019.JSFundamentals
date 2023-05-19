e9965ff212427e6ce341f62aefb1b1ca00c61b4e
vovanolo <vpankiv@softserveinc.com>
2023-05-19 20:15:49 +0300

    third lesson

--- app.js
+++ app.js
@@ -144,7 +144,7 @@
 
 // if, else, if else, switch
 
-let age = +prompt("How old are you?");
+// let age = +prompt("How old are you?");
 
 // if(age >= 18){
 //     console.log("you are adult");
@@ -173,7 +173,161 @@ let age = +prompt("How old are you?");
 
 
 
+// loops
 
+// while, for, for of, for in
+
+// while
+// let i = 0;
+
+// while(i <= 5){
+//     console.log(i); // 0 1 2 3 4 5
+//     i++; // i = 6
+// }
+
+// for
+
+// for(let i = 5; i >= 0; i--){
+//     console.log(i);
+// }
+
+
+// const users = ["Ivan", "Petro", "Vasya"];
+// // users[0];
+// // console
+
+// for(let i = 0; i < users.length; i++){
+//     console.log(users[i]);
+// }
+
+// for of
+// const users = ["Ivan", "Petro", "Vasya"];
+// const something = "hello"
+
+// for(let element of users){
+//     console.log(element);
+// }
+
+// for in
+
+// const users = ["Ivan", "Petro", "Vasya"];
+
+
+// for(let index in users){
+//     console.log(index);
+// }
+
+// break, continue
+
+
+// const numbers = [1,2,3,4,5,6,7,8,8,8];
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+//     if(numbers[i] === 5){
+//         break;
+//     }
+// }
+
+// Math.min
+// Math.max
+
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] % 2 !== 0){
+//         continue;
+//     }
+//     console.log(numbers[i]);  
+// }
+
+// let repeatEight = 0;
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] === 8){
+//         repeatEight++;
+//     } 
+// }
+// console.log(repeatEight);
+
+
+// functions
+
+// console.log("hello");
+
+// function sayHello(){
+//     console.log("hello");
+// }
+
+
+// sayHello();
+
+// function adding(a,b){
+//     console.log(a + b);
+// }
+
+// adding(10, 20);
+
+
+// function adding(a,b){
+//    return a + b;     
+// }
+
+// // adding(10,20);
+// console.log(adding(10,20));
+
+
+// function multiplication(a,b){
+//     return a * b;
+// }
+
+
+
+// function multiplication(){
+//     // console.log(arguments);
+//     let result = 1;
+//     for(let i = 0; i < arguments.length; i++){
+//         result *= arguments[i];
+//     }
+//     console.log(result);
+
+// }
+
+// multiplication(1,2,3,4,5,6,7);
+
+
+// let x = 1;
+
+// function something(){
+//     let x = "hello";
+//     console.log(x);
+// }
+// something();
+// console.log(x);
+
+
+// function declaration
+// sayHello();
+
+// function sayHello(){
+//     console.log("hello");
+// }
+
+// function expression
+// hello();
+
+// const hello = function(){
+//     console.log("hello");
+// }
+
+// методи перебору масиву
+// const users = ["Ivan", "Petro", "Vasya"];
+
+// map
+// let newUsers = users.map(function(element, index){
+//     // console.log(element);
+//         return "2" + element;
+//     // console.log(index);
+// })
+// console.log(newUsers);
+// sort(function(a,b) return b - a;)
 
 
 
